46262aa252aff94e77f6095313e5bea3e14a9804
pavelshermayster <shermayster@gmail.com>
2021-02-13 11:09:32 +0200

    fix: compilation

--- P02-VehiclesAPI/eureka/src/main/java/com/udacity/eureka/EurekaApplication.java
+++ P02-VehiclesAPI/eureka/src/main/java/com/udacity/eureka/EurekaApplication.java
@@ -1,14 +1,17 @@
 package com.udacity.eureka;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.boot.SpringApplication;
 
+import static org.springframework.boot.SpringApplication.*;
 
-@SpringBootApplication
 @EnableEurekaServer
+@SpringBootApplication
 public class EurekaApplication {
-   public static void main(String[] args) {
-       SpringApplication.run(EurekaApplication.class, args);
-   } 
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaApplication.class, args);
+	}
+
 }
