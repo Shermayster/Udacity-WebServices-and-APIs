f48974fa73d7e398fe9ecc5bf67eb705feb76dbf
pavelshermayster <shermayster@gmail.com>
2021-02-14 19:46:03 +0200

    fix: car service api

--- P02-VehiclesAPI/vehicles-api/src/main/java/com/udacity/vehicles/service/CarService.java
+++ P02-VehiclesAPI/vehicles-api/src/main/java/com/udacity/vehicles/service/CarService.java
@@ -1,34 +1,36 @@
 package com.udacity.vehicles.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import com.udacity.vehicles.client.maps.MapsClient;
-import com.udacity.vehicles.client.prices.Price;
 import com.udacity.vehicles.client.prices.PriceClient;
 import com.udacity.vehicles.domain.Location;
 import com.udacity.vehicles.domain.car.Car;
 import com.udacity.vehicles.domain.car.CarRepository;
-import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 /**
- * Implements the car service create, read, update or delete
- * information about vehicles, as well as gather related
- * location and price data when desired.
+ * Implements the car service create, read, update or delete information about
+ * vehicles, as well as gather related location and price data when desired.
  */
 @Service
 public class CarService {
 
     private final CarRepository repository;
-    private final MapsClient mapsClient;
     private final PriceClient priceClient;
+    private final MapsClient mapsClient;
 
-    public CarService(CarRepository repository, MapsClient mapsClient, PriceClient priceClient) {
+    public CarService(CarRepository repository, PriceClient priceClient, MapsClient mapsClient) {
         this.repository = repository;
-        this.mapsClient = mapsClient;
         this.priceClient = priceClient;
+        this.mapsClient = mapsClient;
     }
 
     /**
      * Gathers a list of all vehicles
+     * 
      * @return a list of all vehicles in the CarRepository
      */
     public List<Car> list() {
@@ -37,36 +39,37 @@ public class CarService {
 
     /**
      * Gets car information by ID (or throws exception if non-existent)
+     * 
      * @param id the ID number of the car to gather information on
      * @return the requested car's information, including location and price
      */
     public Car findById(Long id) throws CarNotFoundException {
-        Car car;
-        try {
-             car = this.repository.getOne(id);
-             String carPrice = priceClient.getPrice(id);
-             car.setPrice(carPrice);
-             Location carLocation = mapsClient.getAddress(car.getLocation());
-             car.setLocation(carLocation);
-        } catch (Exception e) {
+        Optional<Car> optionalCar;
+        optionalCar = this.repository.findById(id);
+        if (optionalCar.isEmpty()) {
             throw new CarNotFoundException();
         }
+        Car car = optionalCar.get();
+        String carPrice = priceClient.getPrice(id);
+        car.setPrice(carPrice);
+        Location carLocation = mapsClient.getAddress(car.getLocation());
+        car.setLocation(carLocation);
         return car;
     }
 
     /**
      * Either creates or updates a vehicle, based on prior existence of car
+     * 
      * @param car A car object, which can be either new or existing
      * @return the new/updated car is stored in the repository
      */
     public Car save(Car car) {
         if (car.getId() != null) {
-            return repository.findById(car.getId())
-                    .map(carToBeUpdated -> {
-                        carToBeUpdated.setDetails(car.getDetails());
-                        carToBeUpdated.setLocation(car.getLocation());
-                        return repository.save(carToBeUpdated);
-                    }).orElseThrow(CarNotFoundException::new);
+            return repository.findById(car.getId()).map(carToBeUpdated -> {
+                carToBeUpdated.setDetails(car.getDetails());
+                carToBeUpdated.setLocation(car.getLocation());
+                return repository.save(carToBeUpdated);
+            }).orElseThrow(CarNotFoundException::new);
         }
 
         return repository.save(car);
@@ -74,6 +77,7 @@ public class CarService {
 
     /**
      * Deletes a given car by ID
+     * 
      * @param id the ID number of the car to delete
      */
     public void delete(Long id) {
